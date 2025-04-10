b6d61b040cb3955624012faa67848595e53ca39a
Sonika <sonika1206@gmail.com>
2025-04-10 06:15:54 -0400

    TabviewController

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+let appColor: UIColor = .systemTeal
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
    let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
         
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -25,9 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
-        window?.rootViewController = loginViewController
+        window?.rootViewController = mainViewController
        // window?.rootViewController = onboardingContainerViewController
-
+        //mainViewController.selectedIndex = 1
         return true
     }
 }
